ab5ebeda94da654995bdfaa74860e674c7f20908
eeeee <elice_huayfoung@hotmail.com>
2018-08-13 01:16:43 +0800

    fix bug

--- src/__tests__/behaviour/TextBoxField.test.js
+++ src/__tests__/behaviour/TextBoxField.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import sinon from 'sinon';
 import TextBoxField from '../../components/TextBoxField';
 
 it('renders without crashing', () => {
